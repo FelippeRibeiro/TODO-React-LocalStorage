056c04795cc54739cd0f0688288d05a0b005c4c0
FelipeRibeiro <felipper43@gmail.com>
2023-11-17 12:11:59 -0300

    wip: animations on delete

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,9 +1,15 @@
+import { useRef } from 'react';
 import { ITodo, TodoStore } from '../context/TodoContext';
 
 export default function Todo({ todo }: { todo: ITodo }) {
+  const Element = useRef<HTMLDivElement>(null);
   const { toggleTodo, removeTodo } = TodoStore();
   return (
-    <div className="shadow-md flex flex-row p-5 items-center justify-between gap-10 border w-[500px] min-h-[100px] animate__animated animate__backInUp">
+    <div
+      ref={Element}
+      className="shadow-md flex flex-row p-5 items-center justify-between gap-10 border w-[500px] min-h-[100px] animate__animated animate__backInUp"
+      style={{ animationDelay: `${Element.current?.offsetTop || 0}ms` }}
+    >
       <div className="flex flex-col">
         <h1 className="text-xl font-semibold text-justify">{todo.title}</h1>
         <cite>{todo.description}</cite>
@@ -11,7 +17,17 @@ export default function Todo({ todo }: { todo: ITodo }) {
 
       <div className="flex flex-row  gap-4">
         <input type="checkbox" checked={todo.completed} className="w-7 h-7" onChange={() => toggleTodo(todo.id)} />
-        <button className="text-red-500 w-7 h-7" onClick={() => removeTodo(todo.id)}>
+        <button
+          className="text-red-500 w-7 h-7"
+          onClick={() => {
+            if (window.confirm('Are you sure you want to delete this todo?')) {
+              Element.current?.classList.add('animate__bounceOut');
+              setTimeout(() => {
+                removeTodo(todo.id);
+              }, 1000);
+            }
+          }}
+        >
           <img src="/trash-2.svg" alt="" />
         </button>
       </div>
