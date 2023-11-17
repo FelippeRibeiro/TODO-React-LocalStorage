0cbb2a14c81b3005ddaf38242c9d96d4c20bc963
FelipeRibeiro <felipper43@gmail.com>
2023-11-17 12:08:01 -0300

    feat: add animations

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -3,7 +3,7 @@ import { ITodo, TodoStore } from '../context/TodoContext';
 export default function Todo({ todo }: { todo: ITodo }) {
   const { toggleTodo, removeTodo } = TodoStore();
   return (
-    <div className="shadow-md flex flex-row p-5 items-center justify-between gap-10 border w-[500px] min-h-[100px]">
+    <div className="shadow-md flex flex-row p-5 items-center justify-between gap-10 border w-[500px] min-h-[100px] animate__animated animate__backInUp">
       <div className="flex flex-col">
         <h1 className="text-xl font-semibold text-justify">{todo.title}</h1>
         <cite>{todo.description}</cite>
